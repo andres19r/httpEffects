52ff939fa32c150ba9620db2a39c333d40071820
Andres Rivero <andres19rivero@gmail.com>
2023-01-31 23:58:41 -0400

    feat: show users in list page

--- src/app/users/list/list.component.ts
+++ src/app/users/list/list.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  users$: Observable<User[]> = this.userService.getUsers();
 
   constructor(private userService: UserService) {}
 
-  ngOnInit(): void {
-    this.userService.getUsers().subscribe(console.log)
-  }
+  ngOnInit(): void {}
 }
