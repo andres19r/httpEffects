ec9668a4062095488c148d90acce1698d6ce5f6d
Andres Rivero <andres19rivero@gmail.com>
2023-02-01 22:44:39 -0400

    feat: create users reducer

---
+++ src/app/store/reducers/users.reducer.ts
@@ -0,0 +1,34 @@
+import { createReducer, on } from '@ngrx/store';
+import { User } from 'src/app/models/user.model';
+import { loadUsers, loadUsersError, loadUsersSuccess } from '../actions';
+
+export interface UsersState {
+  users: User[];
+  loaded: boolean;
+  loading: boolean;
+  error: any;
+}
+
+export const usersInitialState: UsersState = {
+  users: [],
+  loaded: false,
+  loading: false,
+  error: null,
+};
+
+export const usersReducer = createReducer(
+  usersInitialState,
+  on(loadUsers, (state) => ({ ...state, loding: true })),
+  on(loadUsersSuccess, (state, { users }) => ({
+    ...state,
+    loading: false,
+    loaded: true,
+    users: [...users],
+  })),
+  on(loadUsersError, (state, { payload }) => ({
+    ...state,
+    loading: false,
+    loaded: false,
+    error: payload,
+  }))
+);
