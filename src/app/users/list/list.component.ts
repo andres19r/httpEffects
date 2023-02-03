0a9676b143e57a70b9618abb9cffe1a35fcc2d62
Andres Rivero <andres19rivero@gmail.com>
2023-02-02 22:54:39 -0400

    feat: complete effect by returning an actions to get users

--- src/app/users/list/list.component.ts
+++ src/app/users/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Subject, takeUntil } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { loadUsers } from 'src/app/store/actions';
 import { AppState } from 'src/app/store/app.reducers';
@@ -9,12 +10,22 @@ import { AppState } from 'src/app/store/app.reducers';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
   users: User[] = [];
+  private _unsubscribe$ = new Subject<void>();
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.store.dispatch(loadUsers())
+    this.store
+      .select('users')
+      .pipe(takeUntil(this._unsubscribe$))
+      .subscribe(({ users }) => (this.users = users));
+    this.store.dispatch(loadUsers());
+  }
+
+  ngOnDestroy() {
+    this._unsubscribe$.next();
+    this._unsubscribe$.complete();
   }
 }
